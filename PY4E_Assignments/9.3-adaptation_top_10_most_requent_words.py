800d4ccd285d50826dda1949805c9f5cd4d1c3b7
Alex Holmes <alexgholmes@hotmail.com>
2021-04-08 16:02:12 +0930

    found out about lambda, this method works but not as familiar with it yet

--- PY4E_Assignments/9.3-adaptation_top_10_most_requent_words.py
+++ PY4E_Assignments/9.3-adaptation_top_10_most_requent_words.py
@@ -11,6 +11,6 @@ for line in handle:
         #and assign to the dictionary.
         counts[word] = counts.get(word, 0)+ 1
 
-#print(sorted(counts.items(), key=lambda var: var[1], reverse=True))
+print(sorted(counts.items(), key=lambda var: var[1], reverse=True)) #this uses lambda to present in correct variable element order 
 
-print(sorted([(val, key) for key, val in counts.items()], reverse=True)) #returns a sorted list in one line, but not presented with the value first then key.
+#print(sorted([(val, key) for key, val in counts.items()], reverse=True)) #returns a sorted list in one line, but not presented with the value first then key.
